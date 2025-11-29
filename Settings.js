d04870b61a903d3f87719b7103006181de74d0f4
HAMZA 458 <hgfbbkj0@gmail.com>
2025-11-30 02:15:15 +0500

    Add configuration settings for the bot

---
+++ Settings.js
@@ -0,0 +1,10 @@
+//=====💫 HAMZA 458🔥====
+
+module.exports = {
+  BOT_NAME: 'HAMZA-458', // ur bot name
+  OWNER_NAME: 'HAMZA H.S.3', //your name
+  OWNER_NUMBER: '923470807458', // add your phone number
+  SESSION_ID: '', // use sessin id if inbuilt pairing not working
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Harare', // ur timezone
+};
